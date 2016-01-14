f6a51b1c61afff6c2235bc27c2eda283edbf00c9
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-01-14 14:14:26 -0800

    Propagate errors on non-pull-requests

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
+set -e -x
 
+# Install Miniconda
 curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh;
 bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/anaconda;
 export PATH=$HOME/anaconda/bin:$PATH;
 conda config --add channels omnia;
 conda install -yq conda-build jinja2 anaconda-client;
 
-#if ! ./conda-build-all --dry-run openmm; then
-brew install -y doxygen;
+# Install OpenMM dependencies that can't be installed through
+# conda package manager (doxygen + CUDA)
+brew install -y doxygen
 curl -O -s http://developer.download.nvidia.com/compute/cuda/7_0/Prod/network_installers/mac/cuda_mac_installer_tk.tar.gz ;
 curl -O -s http://developer.download.nvidia.com/compute/cuda/7_0/Prod/network_installers/mac/cuda_mac_installer_drv.tar.gz ;
 sudo tar -zxf cuda_mac_installer_tk.tar.gz -C /;
 sudo tar -zxf cuda_mac_installer_drv.tar.gz -C /;
-#fi
 
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-  ./conda-build-all $CONDA_BUILD_ALL_FLAGS * || true;
-else
-  ./conda-build-all $CONDA_BUILD_ALL_FLAGS *;
-fi;
+
+# Build packages
+./conda-build-all $CONDA_BUILD_ALL_FLAGS *;
