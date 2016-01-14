afdeff9840ef95383641405e7b2f8e98af3eb216
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-01-13 23:08:47 -0500

    Install osx things.

--- devtools/docker-build-osx.sh
+++ devtools/docker-build-osx.sh
@@ -6,13 +6,13 @@ export PATH=$HOME/anaconda/bin:$PATH;
 conda config --add channels omnia;
 conda install -yq conda-build jinja2 anaconda-client;
 
-if ! ./conda-build-all --dry-run openmm; then
-    brew install -y doxygen;
-    curl -O -s http://developer.download.nvidia.com/compute/cuda/7_0/Prod/network_installers/mac/cuda_mac_installer_tk.tar.gz ;
-    curl -O -s http://developer.download.nvidia.com/compute/cuda/7_0/Prod/network_installers/mac/cuda_mac_installer_drv.tar.gz ;
-    sudo tar -zxf cuda_mac_installer_tk.tar.gz -C /;
-    sudo tar -zxf cuda_mac_installer_drv.tar.gz -C /;
-fi
+#if ! ./conda-build-all --dry-run openmm; then
+brew install -y doxygen;
+curl -O -s http://developer.download.nvidia.com/compute/cuda/7_0/Prod/network_installers/mac/cuda_mac_installer_tk.tar.gz ;
+curl -O -s http://developer.download.nvidia.com/compute/cuda/7_0/Prod/network_installers/mac/cuda_mac_installer_drv.tar.gz ;
+sudo tar -zxf cuda_mac_installer_tk.tar.gz -C /;
+sudo tar -zxf cuda_mac_installer_drv.tar.gz -C /;
+#fi
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
   ./conda-build-all $UPLOAD * || true;
