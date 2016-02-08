a911fbb3cab4ef54ceeed0838f852a523a91f95f
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-02-08 00:25:27 -0500

    More brew madness

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -10,16 +10,16 @@ conda install -yq conda-build jinja2 anaconda-client;
 
 # Install OpenMM dependencies that can't be installed through
 # conda package manager (doxygen + CUDA)
-brew install -y doxygen
+brew install -y --quiet doxygen
 curl -O -s http://developer.download.nvidia.com/compute/cuda/7.5/Prod/network_installers/mac/x86_64/cuda_mac_installer_tk.tar.gz
 curl -O -s http://developer.download.nvidia.com/compute/cuda/7.5/Prod/network_installers/mac/x86_64/cuda_mac_installer_drv.tar.gz
 sudo tar -zxf cuda_mac_installer_tk.tar.gz -C /;
 sudo tar -zxf cuda_mac_installer_drv.tar.gz -C /;
 
 # Install latex.
-brew update -y
-brew tap -y Caskroom/cask
-brew cask install -y mactex
+brew update -y --quiet
+brew tap -y --quiet Caskroom/cask
+brew cask install -y --quiet mactex
 
 # Build packages
 ./conda-build-all $CONDA_BUILD_ALL_FLAGS *;
