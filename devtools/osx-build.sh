89f6c121ee2a77ee0dcb7c2e4e2d5c71b9a40612
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-10-14 20:50:03 -0400

    Update osx build to CUDA 8.0

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -9,13 +9,13 @@ curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.s
 bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/anaconda;
 export PATH=$HOME/anaconda/bin:$PATH;
 conda config --add channels omnia;
-conda install -yq conda-build=2.0.1 jinja2 anaconda-client;
+conda install -yq conda-build jinja2 anaconda-client;
 
 # Install OpenMM dependencies that can't be installed through
 # conda package manager (doxygen + CUDA)
 brew install -y --quiet doxygen
-curl -O -s http://developer.download.nvidia.com/compute/cuda/7.5/Prod/network_installers/mac/x86_64/cuda_mac_installer_tk.tar.gz
-curl -O -s http://developer.download.nvidia.com/compute/cuda/7.5/Prod/network_installers/mac/x86_64/cuda_mac_installer_drv.tar.gz
+curl -O -s http://developer.download.nvidia.com/compute/cuda/8.0/Prod/network_installers/mac/x86_64/cuda_mac_installer_tk.tar.gz
+curl -O -s http://developer.download.nvidia.com/compute/cuda/8.0/Prod/network_installers/mac/x86_64/cuda_mac_installer_drv.tar.gz
 sudo tar -zxf cuda_mac_installer_tk.tar.gz -C /;
 sudo tar -zxf cuda_mac_installer_drv.tar.gz -C /;
 rm -f cuda_mac_installer_tk.tar.gz cuda_mac_installer_drv.tar.gz
