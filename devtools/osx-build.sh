7b90150c6e700d6bb1480b4be19b6d51d7ce3e83
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-11-13 11:40:01 -0500

    Fix osx-cask

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -2,7 +2,9 @@
 set -e -x
 
 # Update homebrew
+brew uninstall -y brew-cask || brew untap -y caskroom/cask || 1
 brew update -y --quiet
+brew tap -y caskroom/cask
 
 # Install Miniconda
 curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh;
