33df1895f5545a0b9fdf91a5eceae8b86a843f37
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-12-19 23:31:17 -0500

    Compiling updates for 7.1.0 beta build

--- openmm/build.sh
+++ openmm/build.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+# Fix hbb issues
+ln -s /opt/rh/devtoolset-2/root/usr/lib/gcc/x86_64-CentOS-linux/ /opt/rh/devtoolset-2/root/usr/lib/gcc/x86_64-redhat-linux
+ln -s /opt/rh/devtoolset-2/root/usr/include/c++/4.8.2/x86_64-CentOS-linux/ /opt/rh/devtoolset-2/root/usr/include/c++/4.8.2/x86_64-redhat-linux
+
+# holy build box paths
+export HBB_PREFIX="/hbb_shlib"
+export PATH=$HBB_PREFIX/bin:/hbb/bin:$PATH
+export C_INCLUDE_PATH=$HBB_PREFIX/include
+export CPLUS_INCLUDE_PATH=$HBB_PREFIX/include
+export LIBRARY_PATH=$HBB_PREFIX/lib
+export PKG_CONFIG_PATH=$HBB_PREFIX/lib/pkgconfig:/usr/lib/pkgconfig
+
+export CPPFLAGS="-I$HBB_PREFIX/include"
+export LDPATHFLAGS="-L$HBB_PREFIX/lib"
+export MINIMAL_CFLAGS="-g -O3 $CPPFLAGS"
+
+export CFLAGS="$MINIMAL_CFLAGS"
+export CXXFLAGS="$MINIMAL_CFLAGS"
+export LDFLAGS="$LDPATHFLAGS -static-libstdc++"
+export STATICLIB_CFLAGS="$MINIMAL_CFLAGS -fPIC"
+export STATICLIB_CXXFLAGS="$MINIMAL_CFLAGS -fPIC"
+export SHLIB_CFLAGS="$MINIMAL_CFLAGS"
+export SHLIB_CXXFLAGS="$MINIMAL_CFLAGS"
+export SHLIB_LDFLAGS="$LDPATHFLAGS -static-libstdc++"
+
+# Clang paths
+export CLANG_PREFIX="/opt/clang/bin/"
+export PATH=$CLANG_PREFIX/bin:$PATH
+
+# OpenMM paths
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF"
 
 # Ensure we build a release
@@ -14,10 +44,16 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=${CUDA_PATH}/"
     CMAKE_FLAGS+=" -DCUDA_TOOLKIT_INCLUDE=${CUDA_PATH}/include"
     CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH}/"
-    CMAKE_FLAGS+=" -DCMAKE_CXX_FLAGS_RELEASE=-I/usr/include/nvidia/"
+    CMAKE_FLAGS+=" -DCMAKE_CXX_FLAGS=-I/usr/include/nvidia/"
+    # Use clang 3.8.1 inside omnia-build-box docker image
+    CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
     # AMD APP SDK 3.0 OpenCL
     CMAKE_FLAGS+=" -DOPENCL_INCLUDE_DIR=/opt/AMDAPPSDK-3.0/include/"
     CMAKE_FLAGS+=" -DOPENCL_LIBRARY=/opt/AMDAPPSDK-3.0/lib/x86_64/libOpenCL.so"
+    # Don't build tests or examples
+    CMAKE_FLAGS+=" -DOPENMM_BUILD_CUDA_TESTS=off"
+    CMAKE_FLAGS+=" -DOPENMM_BUILD_EXAMPLES=off"
+    CMAKE_FLAGS+=" -DOPENMM_BUILD_OPENCL_TESTS=off"
     # CUDA OpenCL
     #CMAKE_FLAGS+=" -DOPENCL_INCLUDE_DUR=${CUDA_PATH}/include/"
     #CMAKE_FLAGS+=" -DOPENCL_LIBRARY=${CUDA_PATH}/lib64/libOpenCL.so"
