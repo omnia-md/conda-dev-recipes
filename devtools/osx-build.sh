8cd67c10c7f5f344bc0ec60dcf4d62f4c6286c0c
Levi Naden <levi.naden@choderalab.org>
2017-04-24 13:54:19 -0400

    Re-re-order the package update order

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -10,9 +10,12 @@ brew tap -y caskroom/cask
 curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh;
 bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/anaconda;
 export PATH=$HOME/anaconda/bin:$PATH;
+conda config --add channels conda-forge;
 conda config --add channels omnia;
 conda config --show;
-conda install -yq conda-build jinja2 anaconda-client;
+conda install -yq conda conda-build jinja2 anaconda-client;
+# set the priorities after the conda update to ensure conda-related packages came from same source.
+conda config --set channel_priority false;
 
 #export INSTALL_CUDA=`./conda-build-all --dry-run -- openmm`
 export INSTALL_OPENMM_PREREQUISITES=true
@@ -36,7 +39,4 @@ if [ "$INSTALL_OPENMM_PREREQUISITES" = true ] ; then
 fi;
 
 # Build packages
-conda config --set channel_priority false;
-conda config --add channels conda-forge;
-conda config --show;
 ./conda-build-all -vvv --python $PY_BUILD_VERSION $UPLOAD -- *
