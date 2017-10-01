17c1a40d7eddc26560b27fd1e72434a50c90f4bd
John Chodera <john.chodera@choderalab.org>
2017-10-01 11:11:54 -0400

    Include dev channel label in build dependencies

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -5,6 +5,7 @@ conda config --add channels omnia
 # Move the conda-forge channel to the top
 # Cannot just append omnia otherwise default would have higher priority
 conda config --add channels conda-forge
+conda config --add channels omnia/label/dev
 conda install -yq conda\>=4.3 conda-build==2.1.17 jinja2 anaconda-client
 
 /io/conda-build-all -vvv --python $PY_BUILD_VERSION $UPLOAD -- /io/*
