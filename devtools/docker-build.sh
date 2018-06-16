dfb39c3e66a849093610cc9b5b8e1f30f930cf27
John Chodera <john.chodera@choderalab.org>
2018-06-16 11:36:25 -0400

    Update for new jchodera/omnia-linux-anvil:texlive18-cuda* docker build images

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 echo "CUDA_VERSION: $CUDA_VERSION"
+echo "PATH: $PATH"
 
 set -e
 set -x
-conda config --add channels omnia
+
+# Activate
+echo "Activating compiler toolchain"
+source /opt/docker/bin/entrypoint_source
+echo "PATH: $PATH"
+
+
+# Make sure we are using the
+echo "Adding conda to path"
+export PATH="/opt/conda/bin:$PATH"
+echo "PATH: $PATH"
+
+#conda config --add channels omnia
 # Move the conda-forge channel to the top
 # Cannot just append omnia otherwise default would have higher priority
-conda config --add channels conda-forge
-conda install -yq conda\<=4.3.34
+#conda config --add channels conda-forge
+#conda install -yq conda\<=4.3.34
 conda config --add channels omnia/label/dev
-conda install -yq conda-build==2.1.17 jinja2 anaconda-client
+#conda install -yq conda-build==2.1.17 jinja2 anaconda-client
 
 /io/conda-build-all -vvv --python $PY_BUILD_VERSION $UPLOAD -- /io/*
 
