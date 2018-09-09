9b2da8da44a3af38127faff245dc2c09009e2497
John Chodera <john.chodera@choderalab.org>
2018-09-09 11:37:07 -0400

    Check against relevant beta and dev channels for osx build

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -78,5 +78,5 @@ conda config --add channels omnia/label/devcuda${CUDA_SHORT_VERSION};
 
 #for PY_BUILD_VERSION in "27" "35" "36" "37"; do
 for PY_BUILD_VERSION in "27" "35" "36"; do
-    ./conda-build-all -vvv --python $PY_BUILD_VERSION --numpy "1.15" $UPLOAD -- *
+    ./conda-build-all -vvv --python $PY_BUILD_VERSION --check-against omnia/label/beta --check-against omnia/label/betacuda${CUDA_SHORT_VERSION} --check-against omnia/label/dev --check-against omnia/label/devcuda${CUDA_SHORT_VERSION} --numpy "1.15" $UPLOAD -- *
 done
