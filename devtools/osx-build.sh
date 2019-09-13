705be640401b3f53df48937ffa38a9f55c7b202e
peastman <peastman@stanford.edu>
2019-09-13 11:37:00 -0700

    Fix mac builds

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -8,8 +8,8 @@ rm -rf /usr/local/var/homebrew/locks
 brew update
 
 # Install Miniconda
-curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh;
-bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/anaconda;
+curl -s -O https://repo.continuum.io/miniconda/Miniconda3-4.6.14-MacOSX-x86_64.sh;
+bash Miniconda3-4.6.14-MacOSX-x86_64.sh -b -p $HOME/anaconda;
 export PATH=$HOME/anaconda/bin:$PATH;
 conda config --add channels conda-forge
 conda config --add channels omnia
