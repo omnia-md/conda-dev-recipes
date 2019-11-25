0592e6892e821e4c68f4cb4a5e3e3296f72b14c2
Levi Naden <lnaden@vt.edu>
2019-11-25 10:00:44 -0500

    Fix another set(none) issue and add new --scheduled-only flag and mechanism to flag builds

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Channels are added in FIFO order here
 conda config --add channels omnia
+conda config --add channels conda-forge
 
 conda update --yes conda conda-build anaconda-client
 
 chmod +x /io/conda-build-all
 
-/io/conda-build-all $CBA_FLAGS -m /conda_configs/$CONFIG.yaml --dry-run -- /io/*
+/io/conda-build-all $CBA_FLAGS -m /conda_configs/$CONFIG.yaml -- /io/*
 
 
 ##!/bin/bash
